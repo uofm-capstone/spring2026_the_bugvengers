647e2e5ff856d9be8264f36c7c854b877ae5930e
Jonathan Maronde <j2mike4@gmail.com>
2026-03-03 13:29:53 -0600

    Deployed sites are allowed to be hosts now according to the production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << /\.run\.app\z/
 
+  # allows deployed sites to be a host and not be blocked by rails' security features
+  config.hosts << /.*\.us-central1\.run\.app/
+
   # Show full error reports even in production (for debugging only!)
   config.consider_all_requests_local = true
 

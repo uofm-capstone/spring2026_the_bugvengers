38f9776681545d1c9831f2399fad3aaa8a41f79b
Brandon Rodriguez <brandon.rodriguez1016@outlook.com>
2026-04-28 15:58:16 -0500

    Fixed bug issue.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_04_14_194751) do
+ActiveRecord::Schema[7.0].define(version: 2026_04_23_203518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -152,6 +152,7 @@ ActiveRecord::Schema[7.0].define(version: 2026_04_14_194751) do
     t.boolean "is_active", default: false, null: false
     t.boolean "temp_password_changed", default: false, null: false
     t.datetime "last_login_at"
+    t.datetime "date_joined"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
